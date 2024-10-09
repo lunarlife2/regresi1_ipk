1adbcb245090c314ba7316e33c980ea20ed8625f
Averina Harseno Subianto <156516558+lunarlife2@users.noreply.github.com>
2024-10-09 15:44:59 +0700

    Update Tugas_Modul3_B_Verin_11757.py

--- Tugas_Modul3_B_Verin_11757.py
+++ Tugas_Modul3_B_Verin_11757.py
@@ -72,8 +72,7 @@ if uploaded_file is not None:
     st.write("<h3 style='text-align: center; color:#0073e6'>Data yang diupload: </h3>", unsafe_allow_html=True)
     st.dataframe(input_data)
 
-    model_directory = r'D:\atma\Semester 5\ML\Supervised Learning (Praktek)\Model Suprevised Learning (Praktek)\Tugas3_B_11757'
-    model_path = os.path.join(model_directory, r'SVR_IPK_model.pkl')
+    model_path = r'SVR_IPK_model.pkl'
 
     if os.path.exists(model_path):
         with open(model_path, 'rb') as f:
